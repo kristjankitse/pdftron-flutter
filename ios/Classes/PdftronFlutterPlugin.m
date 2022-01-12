9c67cf02b74c19353a629d1123cae9515e8d1fb5
kristjanbauhub <97598323+kristjanbauhub@users.noreply.github.com>
2022-01-12 14:13:09 +0200

    color test

--- ios/Classes/PdftronFlutterPlugin.m
+++ ios/Classes/PdftronFlutterPlugin.m
@@ -2705,7 +2705,7 @@
         
         if ([tool isKindOfClass:[BauhubTaskTool class]]) {
             NSString *colorCode = [toolMode substringFromIndex: [toolMode length] - 6];
-            [((BauhubTaskTool *)tool) setTaskImageName:@"task_FFEBFD"];
+            [((BauhubTaskTool *)tool) setTaskImageName:[NSString stringWithFormat:@"task_FFEBFD"]];
         }
     }
 
